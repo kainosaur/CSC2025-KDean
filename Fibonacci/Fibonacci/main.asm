63978d4b337eb8e8ad80e35f855008bc58913d83
Kain <152326772+kainosaur@users.noreply.github.com>
2025-10-01 11:53:27 -0600

    Fibonacci without loop

---
+++ Fibonacci/Fibonacci/main.asm
@@ -0,0 +1,39 @@
+.386P
+.model flat
+extern _ExitProcess@4:near
+
+.data
+
+.stack
+
+.code
+
+
+main PROC
+	;Take the value of n
+	mov eax, 7 ; ecx used for loop counter
+	mov edx, 1 ; previous
+	mov esi, 1 ; current
+	mov edi, 1 ; next
+
+	mov ecx, eax
+	dec ecx
+	dec ecx
+	cmp ecx, 0
+	jle finish
+	begin:
+		add edi, edx
+		mov edx, esi
+		mov esi, edi
+		cmp ecx, 0
+		dec ecx
+	jg begin
+	
+	finish:
+	mov eax, edi
+	push eax
+	call _ExitProcess@4
+main ENDP
+END main
+
+
